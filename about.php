7804adbf817f053a419f3cb381b6381a9607beab
Nhut Truong <104214211+Nhuttruong61@users.noreply.github.com>
2022-11-16 13:08:02 +0700

    update

--- about.php
+++ about.php
@@ -9,13 +9,13 @@
     <meta name="keywords" content="T-P Watch" />
     <link rel="icon" type="image/x-icon" href="./images/icon.png">
     <script>
-    addEventListener("load", function() {
-        setTimeout(hideURLbar, 0);
-    }, false);
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
 
-    function hideURLbar() {
-        window.scrollTo(0, 1);
-    }
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
     </script>
     <!-- //Meta tag Keywords -->
 
@@ -33,12 +33,8 @@
     <!-- //Custom-Files -->
 
     <!-- web fonts -->
-    <link
-        href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext"
-        rel="stylesheet">
-    <link
-        href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
-        rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
     <!-- //web fonts -->
 
 </head>
@@ -139,13 +135,11 @@
                         </div>
                         <div class="form-group">
                             <label class="col-form-label">Password</label>
-                            <input type="password" class="form-control" placeholder=" " name="Password" id="password1"
-                                required="">
+                            <input type="password" class="form-control" placeholder=" " name="Password" id="password1" required="">
                         </div>
                         <div class="form-group">
                             <label class="col-form-label">Confirm Password</label>
-                            <input type="password" class="form-control" placeholder=" " name="Confirm Password"
-                                id="password2" required="">
+                            <input type="password" class="form-control" placeholder=" " name="Confirm Password" id="password2" required="">
                         </div>
                         <div class="right-w3l">
                             <input type="submit" class="form-control" value="Register">
@@ -184,8 +178,7 @@
                         <!-- search -->
                         <div class="col-10 agileits_search">
                             <form class="form-inline" action="#" method="post">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Tìm kiếm sản phẩm"
-                                    aria-label="Search" required>
+                                <input class="form-control mr-sm-2" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search" required>
                                 <button class="btn my-2 my-sm-0" type="submit">Tìm kiếm</button>
                             </form>
                         </div>
@@ -224,9 +217,7 @@
                         </select>
                     </form>
                 </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -241,14 +232,12 @@
                         $sql_category_danhmuc = mysqli_query($con, 'SELECT * FROM tbl_category ORDER BY category_id DESC');
                         while ($row_category_danhmuc = mysqli_fetch_array($sql_category_danhmuc)) {
                         ?>
-                        <li class="nav-item  mr-lg-2 mb-lg-0 mb-2">
-                            <a class="nav-link "
-                                href="index.php?quanly=danhmuc&id=<?php echo $row_category_danhmuc['category_id'] ?>"
-                                role="button" aria-haspopup="true" aria-expanded="false">
-                                <?php echo $row_category_danhmuc['category_name'] ?>
-                            </a>
+                            <li class="nav-item  mr-lg-2 mb-lg-0 mb-2">
+                                <a class="nav-link " href="index.php?quanly=danhmuc&id=<?php echo $row_category_danhmuc['category_id'] ?>" role="button" aria-haspopup="true" aria-expanded="false">
+                                    <?php echo $row_category_danhmuc['category_name'] ?>
+                                </a>
 
-                        </li>
+                            </li>
                         <?php
                         }
                         ?>
@@ -257,26 +246,23 @@
                             $sql_danhmuctin = mysqli_query($con, "SELECT * FROM tbl_danhmuc_tin ORDER BY danhmuctin_id DESC");
 
                             ?>
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Tin tức
                             </a>
                             <div class="dropdown-menu">
                                 <?php
                                 while ($row_danhmuctin = mysqli_fetch_array($sql_danhmuctin)) {
                                 ?>
-                                <a class="dropdown-item"
-                                    href="index.php?quanly=tintuc&id_tin=<?php echo $row_danhmuctin['danhmuctin_id'] ?>">
-                                    <?php echo $row_danhmuctin['tendanhmuc'] ?>
-                                </a>
+                                    <a class="dropdown-item" href="index.php?quanly=tintuc&id_tin=<?php echo $row_danhmuctin['danhmuctin_id'] ?>">
+                                        <?php echo $row_danhmuctin['tendanhmuc'] ?>
+                                    </a>
                                 <?php
                                 }
                                 ?>
                             </div>
                         </li>
                         <li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Trang
                             </a>
                             <div class="dropdown-menu">
@@ -354,12 +340,13 @@
         <div class="container py-xl-4 py-lg-2">
             <!-- tittle heading -->
             <h3 class="tittle-w3l text-center text-white mb-lg-5 mb-sm-4 mb-3">
-                <span>K</span>ách
+                <span>K</span>hách
                 <span>H</span>àng
                 <span>N</span>ói
                 <span>V</span>ề
                 <span>T</span>
                 <span>-</span>
+                <span>P</span>
                 <span>W</span>atch
 
             </h3>
@@ -574,8 +561,7 @@
                                     </li>
                                     <li>
                                         <a class="icon gp" href="https://www.instagram.com/thuan.phatt/">
-                                            <i class="fab fa-instagram"
-                                                style="background: url(./images/bg-instagram.jpg);"></i>
+                                            <i class="fab fa-instagram" style="background: url(./images/bg-instagram.jpg);"></i>
 
                                         </a>
                                     </li>
@@ -610,82 +596,82 @@
 
     <!-- nav smooth scroll -->
     <script>
-    $(document).ready(function() {
-        $(".dropdown").hover(
-            function() {
-                $('.dropdown-menu', this).stop(true, true).slideDown("fast");
-                $(this).toggleClass('open');
-            },
-            function() {
-                $('.dropdown-menu', this).stop(true, true).slideUp("fast");
-                $(this).toggleClass('open');
-            }
-        );
-    });
+        $(document).ready(function() {
+            $(".dropdown").hover(
+                function() {
+                    $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+                    $(this).toggleClass('open');
+                },
+                function() {
+                    $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+                    $(this).toggleClass('open');
+                }
+            );
+        });
     </script>
     <!-- //nav smooth scroll -->
 
     <!-- popup modal (for location)-->
     <script src="js/jquery.magnific-popup.js"></script>
     <script>
-    $(document).ready(function() {
-        $('.popup-with-zoom-anim').magnificPopup({
-            type: 'inline',
-            fixedContentPos: false,
-            fixedBgPos: true,
-            overflowY: 'auto',
-            closeBtnInside: true,
-            preloader: false,
-            midClick: true,
-            removalDelay: 300,
-            mainClass: 'my-mfp-zoom-in'
-        });
+        $(document).ready(function() {
+            $('.popup-with-zoom-anim').magnificPopup({
+                type: 'inline',
+                fixedContentPos: false,
+                fixedBgPos: true,
+                overflowY: 'auto',
+                closeBtnInside: true,
+                preloader: false,
+                midClick: true,
+                removalDelay: 300,
+                mainClass: 'my-mfp-zoom-in'
+            });
 
-    });
+        });
     </script>
     <!-- //popup modal (for location)-->
 
     <!-- cart-js -->
     <script src="js/minicart.js"></script>
     <script>
-    paypals.minicarts
-        .render(); //use only unique class names other than paypals.minicarts.Also Replace same class name in css and minicart.min.js
+        paypals.minicarts
+            .render(); //use only unique class names other than paypals.minicarts.Also Replace same class name in css and minicart.min.js
 
-    paypals.minicarts.cart.on('checkout', function(evt) {
-        var items = this.items(),
-            len = items.length,
-            total = 0,
-            i;
+        paypals.minicarts.cart.on('checkout', function(evt) {
+            var items = this.items(),
+                len = items.length,
+                total = 0,
+                i;
 
-        // Count the number of each item in the cart
-        for (i = 0; i < len; i++) {
-            total += items[i].get('quantity');
-        }
+            // Count the number of each item in the cart
+            for (i = 0; i < len; i++) {
+                total += items[i].get('quantity');
+            }
 
-        if (total < 3) {
-            alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
-            evt.preventDefault();
-        }
-    });
+            if (total < 3) {
+                alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
+                evt.preventDefault();
+            }
+        });
     </script>
     <!-- //cart-js -->
 
     <!-- password-script -->
     <script>
-    window.onload = function() {
-        document.getElementById("password1").onchange = validatePassword;
-        document.getElementById("password2").onchange = validatePassword;
-    }
+        window.onload = function() {
+            document.getElementById("password1").onchange = validatePassword;
+            document.getElementById("password2").onchange = validatePassword;
+        }
 
-    function validatePassword() {
-        var pass2 = document.getElementById("password2").value;
-        var pass1 = document.getElementById("password1").value;
-        if (pass1 != pass2)
-            document.getElementById("password2").setCustomValidity("Passwords Don't Match");
-        else
-            document.getElementById("password2").setCustomValidity('');
-        //empty string means no validation error
-    }
+        function validatePassword() {
+            var pass2 = document.getElementById("password2").value;
+            var pass1 = document.getElementById("password1").value;
+            if (pass1 != pass2)
+                document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+            else
+                document.getElementById("password2").setCustomValidity('');
+            //empty string means no validation error
+        }
     </script>
     <!-- //password-script -->
 
@@ -697,34 +683,34 @@
     <script src="js/move-top.js"></script>
     <script src="js/easing.js"></script>
     <script>
-    jQuery(document).ready(function($) {
-        $(".scroll").click(function(event) {
-            event.preventDefault();
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event) {
+                event.preventDefault();
 
-            $('html,body').animate({
-                scrollTop: $(this.hash).offset().top
-            }, 1000);
+                $('html,body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 1000);
+            });
         });
-    });
     </script>
     <!-- //end-smooth-scrolling -->
 
     <!-- smooth-scrolling-of-move-up -->
     <script>
-    $(document).ready(function() {
-        /*
-        var defaults = {
-        	containerID: 'toTop', // fading element id
-        	containerHoverID: 'toTopHover', // fading element hover id
-        	scrollSpeed: 1200,
-        	easingType: 'linear' 
-        };
-        */
-        $().UItoTop({
-            easingType: 'easeOutQuart'
-        });
+        $(document).ready(function() {
+            /*
+            var defaults = {
+            	containerID: 'toTop', // fading element id
+            	containerHoverID: 'toTopHover', // fading element hover id
+            	scrollSpeed: 1200,
+            	easingType: 'linear' 
+            };
+            */
+            $().UItoTop({
+                easingType: 'easeOutQuart'
+            });
 
-    });
+        });
     </script>
     <!-- //smooth-scrolling-of-move-up -->
 
