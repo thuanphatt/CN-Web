22158737ace513eebe14631bd49cd62d15a73525
thuanphatt <zelorntp@gmail.com>
2022-11-20 23:20:58 +0700

    remove minacart

--- terms.php
+++ terms.php
@@ -657,30 +657,6 @@ session_start();
     </script>
     <!-- //popup modal (for location)-->
 
-    <!-- cart-js -->
-    <script src="js/minicart.js"></script>
-    <script>
-    paypals.minicarts
-        .render(); //use only unique class names other than paypals.minicarts.Also Replace same class name in css and minicart.min.js
-
-    paypals.minicarts.cart.on('checkout', function(evt) {
-        var items = this.items(),
-            len = items.length,
-            total = 0,
-            i;
-
-        // Count the number of each item in the cart
-        for (i = 0; i < len; i++) {
-            total += items[i].get('quantity');
-        }
-
-        if (total < 3) {
-            alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
-            evt.preventDefault();
-        }
-    });
-    </script>
-    <!-- //cart-js -->
 
     <!-- password-script -->
     <script>
