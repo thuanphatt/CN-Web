b23afa77e1dc6e18e1c33cf317dff1539eb14bd9
thuanphatt <zelorntp@gmail.com>
2022-11-21 15:26:43 +0700

    remove library

--- index.php
+++ index.php
@@ -121,29 +121,7 @@ require('db/connect.php');
     </script>
     <!-- //popup modal (for location)-->
 
-    <!-- cart-js -->
-    <script>
-    paypals.minicarts
-        .render();
 
-    paypals.minicarts.cart.on('checkout', function(evt) {
-        var items = this.items(),
-            len = items.length,
-            total = 0,
-            i;
-
-        // Count the number of each item in the cart
-        for (i = 0; i < len; i++) {
-            total += items[i].get('quantity');
-        }
-
-        if (total < 3) {
-            alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
-            evt.preventDefault();
-        }
-    });
-    </script>
-    <!-- //cart-js -->
 
     <!-- password-script -->
     <script>
